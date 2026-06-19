63ee8ae311142afbfe15f0341f8c0a729b26ab68
Afroz256 <nafizafarhana5@gmail.com>
2026-06-19 10:18:31 +0600

    Implement Applet and GameApplet classes
    Added Applet and GameApplet classes with methods.

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,31 @@
 
+class Applet {
+
+    // Base class method
+    void launch() {
+        System.out.println("Applet launched");
+    }
+}
+
+// Derived class
+class GameApplet extends Applet {
+
+    // Method specific to GameApplet
+    void play() {
+        System.out.println("Playing game");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Create GameApplet object
+        GameApplet game = new GameApplet();
+
+        // Call inherited method
+        game.launch();
+
+        // Call derived class method
+        game.play();
+    }
+}
